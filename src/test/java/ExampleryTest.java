1dc7313563a6595ef183cb23c4c52b6b7aada5d8
Maciej Piotrowski <nishi@wimiip.pl>
2016-07-15 21:23:49 +0200

    8.3 Overriding methods.
    JAVA:
    - created Car class with overridng start method from vehicle class
    - added exemplery test to copy it as skeleton for new unit tests
    
    UNIT TEST:
    - created car_CheckingOverridingStartMethodForCars_NothingReturnsOnlyAsserts() test method within OverridingMethodsTest class

---
+++ src/test/java/ExampleryTest.java
@@ -0,0 +1,25 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by nishi on 2016-07-15.
+ */
+public class ExampleryTest {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void test() {
+
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("Cleaning after " + ExampleryTest.class.getSimpleName());
+    }
+
+}
